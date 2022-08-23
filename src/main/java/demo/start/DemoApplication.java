a18a7c02d7471f1fa90d264ac2d7f6c6a2777538
nikingrid <nikpam2008@hotmail.com>
2022-08-23 13:59:22 +0200

    Add instanciar estudiante

--- src/main/java/demo/start/DemoApplication.java
+++ src/main/java/demo/start/DemoApplication.java
@@ -8,6 +8,18 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		
+		Student student1= new Student();
+		
+		student1.setId(100);
+		student1.setFirstName("Carlos");
+		student1.setLastName("Sainz");
+		
+		System.out.println(student1.getId());
+		System.out.println(student1.getFirstName());
+		System.out.println(student1.getLastName());
+		
+		
 	}
 
 }
