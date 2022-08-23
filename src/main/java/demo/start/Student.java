8e038db2de8f513b921c07dacbf6d9f4d52c8e1e
nikingrid <nikpam2008@hotmail.com>
2022-08-23 13:49:39 +0200

    Add todos las dependencias lombok

---
+++ src/main/java/demo/start/Student.java
@@ -0,0 +1,18 @@
+package demo.start;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+
+public class Student {
+	
+	private long id;
+	private String firstName;
+	private String lastName;
+	
+	
+}
